1df31b86b061859dcdad7ccc3eea97779b812d81
Patricia <166408575+Trish-Cal@users.noreply.github.com>
2024-05-14 21:29:54 -0500

    Add files via upload
    code for block07 assignment

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Create a string with the statement by declaring the variable using either let or const, naming the string, and assigning the value by using the equal sign and the statement in quotes
+  Use a command to display the message for the user 
+*/
+let VaultNotice = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log(VaultNotice);
+
+// Create 3 variables, using arithmetic operators for each of the 3 codes in the combination
+
+let Combo1 = 5 + 5;
+let Combo2 = 4 * 10;
+let Combo3 = 40 - 1;
+
+//Create an alert or dialog box displaying the codes
+
+alert(Combo1 + " " + Combo2 + " " + Combo3);
+
